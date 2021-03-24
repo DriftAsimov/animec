603f80007846007a4db6c91e83f86da34f24af2d
DriftAsimov <65854695+DriftAsimov@users.noreply.github.com>
2021-03-24 17:16:54 +0530

    Update __init__.py

--- animec/__init__.py
+++ animec/__init__.py
@@ -6,57 +6,47 @@ from urllib.request import urlopen
 import re
 
 '''
-This is an unofficial module to extract an anime character data from ://myanimelist.net/
+This is an unofficial module to extract an anime character data from https://myanimelist.net/
 '''
 
 __author__ = "DriftAsimov"
 __title__ = "animec"
 __copyright__ = "Copyright Drift Asimov 2021"
-__version__ = "0.0.3"
+__version__ = "0.0.5"
 __license__ = "MIT"
 
 #IMPLEMENTATION
-class Character:
+class charsearch:
     
     def __init__(self, query):
-        self.query = query
 
-    def search(self):
-        
+        for j in search(f"{query} anime character", tld="com", num=50, stop=50, pause=3):
+            if ('myanimelist') in j:
+                url = j
+                break
+
         try:
             
-            result = {}
-
-            for j in search(f"{self.query} anime character", tld="com", num=50, stop=50, pause=3):
-                if 'myanimelist' in j:
-                    url = j
-
-            result["url"] = url
-
             html_page = urlopen(url)
             soup = BeautifulSoup(html_page, 'html.parser')
 
             images = soup.findAll('img')
 
-            string_list = []
-
-            for i in images:
-                string_list.append(str(i))
+            string_list = [str(i) for i in images]
 
             for k in string_list:
                 if 'characters' in k:
                     char = k
+                    break
 
-            image_url = char.split('data-src="')[1][:-3]
-            
-            result["image"] = image_url
+            image_url = re.search("https://.*jpg", char).group()
 
             title = soup.find('h2')
             title = title.get_text()
 
-            result["title"] = title
-
-            return result
+            self.title = title
+            self.url = url
+            self.image_url = image_url
         
-        except Exception as e:
-            print(e)
+        except:
+            print("No such anime character found.")
