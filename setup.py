b23152f2cc86d4d7757e8feb3078f7f1430d5a90
DriftAsimov <65854695+DriftAsimov@users.noreply.github.com>
2021-03-24 17:16:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 from setuptools import setup, find_packages
  
 classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Education',
-  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'Development Status :: 4 - Beta',
+  'Intended Audience :: Developers',
+  'Operating System :: OS Independent',
   'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
+  'Programming Language :: Python :: 3',
+  "Topic :: Education :: Testing",
+  "Topic :: Software Development :: Libraries"
 ]
  
 setup(
   name='animec',
-  version='0.0.2',
-  description='An unofficial module to extract character data from myanimelist',
-  long_description=open('README.md').read(),
+  version='0.0.5',
+  description='An unofficial API to extract character data from myanimelist',
+  long_description=open('README.md', encoding='utf-8').read(),
   long_description_content_type = 'text/markdown',
   url='https://github.com/DriftAsimov/animec',  
   author='DriftAsimov',
   author_email='driftasimov@gmail.com',
   license='MIT', 
   classifiers=classifiers,
-  keywords='animecharacter', 
+  keywords=['animecharacter anime api character myanimelist'], 
   packages=find_packages(),
-  install_requires=['google-search==1.0.2','bs4==0.0.1','urllib3==1.25.9'] 
+  install_requires=['google-search==1.0.2','bs4==0.0.1','urllib3==1.25.9','google==3.0.0'] 
 )
