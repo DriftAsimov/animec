2cd05ea7eb9d762f364297fb635f8b95259a3715
DriftAsimov <65854695+DriftAsimov@users.noreply.github.com>
2021-03-26 12:23:10 +0530

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,16 @@
+import sphinx_rtd_theme
+
+extensions = [
+    "sphinx_rtd_theme",
+    "sphinx.ext.napolean",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc"
+]
+
+master_doc = "index"
+html_theme = "sphinx_rtd_theme"
+
+project = "animec"
+copyright = "Copyright Drift Asimov"
+author = "DriftAsimov"
+version = release = "0.0.6"
