85919d2fb1e8118fe30f96d732d4549c102ca55d
DriftAsimov <65854695+DriftAsimov@users.noreply.github.com>
2021-03-29 14:29:47 +0530

    Update __init__.py

--- animec/__init__.py
+++ animec/__init__.py
@@ -8,5 +8,5 @@ An unofficial API to get data about anime characters, anime news and more.
 __author__ = "DriftAsimov"
 __title__ = "animec"
 __copyright__ = "Copyright Drift Asimov 2021"
-__version__ = "0.0.6"
+__version__ = "0.0.72"
 __license__ = "MIT"
