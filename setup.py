88bb282c3b2205d398491c60515f9b715323a7de
DriftAsimov <65854695+DriftAsimov@users.noreply.github.com>
2021-04-13 10:16:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, find_packages
- 
+
+version = '0.1.0'
+
+with open('requirements.txt', 'r') as f:
+  requirements = f.read().split('\n')
+
+with open('README.md', 'r', encoding = 'utf-8') as f:
+  long_description = f.read()
+
 classifiers = [
   'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Developers',
@@ -11,17 +19,17 @@ classifiers = [
 ]
  
 setup(
-  name='animec',
-  version='0.0.88',
-  description='An unofficial API to get data about anime characters, anime news and more.',
-  long_description=open('README.md', encoding='utf-8').read(),
+  name = 'animec',
+  version = version,
+  description = 'An unofficial API to get data about anime characters, anime news, anime info and more.',
+  long_description = long_description,
   long_description_content_type = 'text/markdown',
-  url='https://github.com/DriftAsimov/animec',  
-  author='DriftAsimov',
-  author_email='driftasimov@gmail.com',
-  license='MIT', 
-  classifiers=classifiers,
-  keywords=['animecharacter anime api character myanimelist news animenews'], 
-  packages=find_packages(),
-  install_requires=['google-search','bs4','urllib3'] 
+  url = 'https://github.com/DriftAsimov/animec',  
+  author = 'DriftAsimov',
+  author_email = 'driftasimov@gmail.com',
+  license = 'MIT', 
+  classifiers = classifiers,
+  keywords = ['animecharacter anime api character myanimelist news animenews animeinfo'], 
+  packages = find_packages(),
+  install_requires = requirements 
 )
