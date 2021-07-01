423cfb60b62cf2f5b2c59731cf7e7750dd11b603
DriftAsimov <65854695+DriftAsimov@users.noreply.github.com>
2021-07-01 11:18:54 +0530

    Bump to version `0.2.0`

--- setup.py
+++ setup.py
@@ -1,40 +1,42 @@
 from setuptools import setup, find_packages
 
-version = '0.1.50'
+version = "0.2.0"
 
-with open('requirements.txt', 'r') as f:
-  requirements = f.read().split('\n')
+with open("requirements.txt", "r") as f:
+    requirements = f.read().split("\n")
 
-with open('README.md', 'r', encoding = 'utf-8') as f:
-  long_description = f.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 classifiers = [
-  'Development Status :: 5 - Production/Stable',
-  'Intended Audience :: Developers',
-  'Operating System :: OS Independent',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3',
-  "Topic :: Education :: Testing",
-  "Topic :: Software Development :: Libraries"
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Topic :: Education :: Testing",
+    "Topic :: Software Development :: Libraries",
 ]
- 
+
 setup(
-  name = 'animec',
-  version = version,
-  description = 'A module to get data about anime characters, news, info, lyrics and more.',
-  long_description = long_description,
-  long_description_content_type = 'text/markdown',
-  url = 'https://github.com/DriftAsimov/animec',  
-  project_urls = {
-    "Documentation" : "https://animec.readthedocs.io/en/latest/",
-    "Issue tracker" : "https://github.com/DriftAsimov/animec/issues",
-    "Github" : "https://github.com/DriftAsimov/animec"
-  },
-  author = 'DriftAsimov',
-  author_email = 'driftasimov@gmail.com',
-  license = 'MIT', 
-  classifiers = classifiers,
-  keywords = ['animecharacter anime character myanimelist news animenews animeinfo music animelyrics lyrics'], 
-  packages = find_packages(),
-  install_requires = requirements 
+    name = "animec",
+    version = version,
+    description = "A module to get data about anime characters, news, info, lyrics and more.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/DriftAsimov/animec",
+    project_urls = {
+        "Documentation": "https://animec.readthedocs.io/en/latest/",
+        "Issue tracker": "https://github.com/DriftAsimov/animec/issues",
+        "Github": "https://github.com/DriftAsimov/animec",
+    },
+    author = "DriftAsimov",
+    author_email = "driftasimov@gmail.com",
+    license = "MIT",
+    classifiers = classifiers,
+    keywords = [
+        "animecharacter anime character myanimelist news animenews animeinfo music animelyrics lyrics"
+    ],
+    packages = find_packages(),
+    install_requires = requirements,
 )
