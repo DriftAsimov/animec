72058e39d3c90771c6610bdc89e7695eaff59ca8
DriftAsimov <65854695+DriftAsimov@users.noreply.github.com>
2021-07-01 11:55:07 +0530

    Bump to version `0.2.0`

--- animec/__init__.py
+++ animec/__init__.py
@@ -9,9 +9,10 @@ A module to get data about anime characters, news, info, lyrics and more.
 from .sagasu import *
 from .aninews import *
 from .anicore import *
+from .errors import *
 
 __author__ = "DriftAsimov"
 __title__ = "animec"
 __copyright__ = "Copyright Drift Asimov 2021"
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 __license__ = "MIT"
