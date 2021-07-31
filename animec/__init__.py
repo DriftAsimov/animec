388532c23c9311ddbcee275d1a305d594cc415dc
DriftAsimov <65854695+DriftAsimov@users.noreply.github.com>
2021-07-31 15:58:09 +0530

    Bump to version `0.3.2`

--- animec/__init__.py
+++ animec/__init__.py
@@ -15,5 +15,5 @@ from .errors import *
 __author__ = "DriftAsimov"
 __title__ = "animec"
 __copyright__ = "Copyright Drift Asimov 2021"
-__version__ = "0.3.0"
+__version__ = "0.3.2"
 __license__ = "MIT"
